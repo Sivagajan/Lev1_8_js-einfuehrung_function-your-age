e71c1c3e1148216b265946fc46c0c349607d447e
Sivagajan <sivagajan@gmail.com>
2022-07-28 14:26:42 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+console.log('works')
+
+let ageCalculator = (yearBorn) => {return 2022 - yearBorn}
+
+console.log(ageCalculator(1989))
+
+let ageDifference = (yearBorn1, yearBorn2) => {return  (yearBorn1 - yearBorn2)}
+
+console.log(ageDifference(1989,1990))
